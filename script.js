8c995283a7ed340fa51c152364db965d96bed9d1
Bertomalone <aprilianoroberto@gmail.com>
2019-03-09 19:20:21 +0800

    computer random images done

--- script.js
+++ script.js
@@ -1,105 +1,159 @@
 function getComputerOptions(params) {
     const comp = Math.random();
 
-    if(comp < 0.34) return 'scissors';
-    if(comp >= 0.34 && comp < 0.67) return 'rock';
+    if(comp < 0.34) return 'cut-hand';
+    if(comp >= 0.34 && comp < 0.67) return 'rock-hand';
 
-    return 'paper';
+    return 'paper-hand';
 }
 
 function getResult(comp, player) {
     if (player == comp) return 'SERI!';
-    if (player == 'scissors') return (comp == 'paper') ? 'MENANG!' : 'KALAH!';
-    if (player == 'paper') return (comp == 'rock') ? 'MENANG!' : 'KALAH!';
-    if (player == 'rock') return (comp == 'scissors') ? 'MENANG!' : 'KALAH!';
+    if (player == 'cut-hand') return (comp == 'paper-hand') ? 'MENANG!' : 'KALAH!';
+    if (player == 'paper-hand') return (comp == 'rock-hand') ? 'MENANG!' : 'KALAH!';
+    if (player == 'rock-hand') return (comp == 'cut-hand') ? 'MENANG!' : 'KALAH!';
 }
 
-const playerScissors = document.querySelector('.scissors');
-playerScissors.addEventListener('click', function () {
-    const computerOptions = getComputerOptions();
-    const playerOptions = playerScissors.className.toString().split(/\s+/)[2];
-    const result = getResult(computerOptions, playerOptions);
-    console.log('comp : ' +computerOptions);
-    console.log('player : ' +playerOptions);
-    console.log('result : ' +result);
+function putar() {
+    const imageComputer = document.querySelector('.board-games__images-computer');
+    const images = ['cut-hand', 'rock-hand', 'paper-hand'];
+    let i = 0;
+    const startTime = new Date().getTime();
 
-    const backgroundComputer = document.querySelector('.board-games__computer-selection');
-    const imgComputer = document.querySelector('.board-games__images-computer');
+    setInterval(function() {
+        if(new Date().getTime() - startTime > 1000) {
+            clearInterval;
+            return;
+        }
 
-    if(computerOptions == 'scissors') {
-        imgComputer.setAttribute('src', 'image/cut-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-scissors');
-        backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
-    } else if(computerOptions == 'rock') {
-        imgComputer.setAttribute('src', 'image/rock-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-rock');
-        backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
-    } else {
-        imgComputer.setAttribute('src', 'image/paper-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-paper');
-        backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
-    }
+        imageComputer.setAttribute('src', 'image/' + images[i++] + '.png');
 
-    const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
-    resultInfo.innerHTML = result;
+        if(i == images.length) i = 0;
+    }, 100)
+
+}
+
+const playerSelectors = document.querySelectorAll('.board-games__player-selection');
+playerSelectors.forEach(function (selectorsItem) {
+    selectorsItem.addEventListener('click', function() {
+        const computerOptions = getComputerOptions();
+        const playerOptions = selectorsItem.className.toString().split(/\s+/)[2];
+        const result = getResult(computerOptions, playerOptions);
+        console.log('comp : ' +computerOptions);
+        console.log('player : ' +playerOptions);
+        console.log('result : ' +result);
+
+        putar();
+
+        const backgroundComputer = document.querySelector('.board-games__computer-selection');
+        const imgComputer = document.querySelector('.board-games__images-computer');
+
+        setTimeout(function() {
+            imgComputer.setAttribute('src', 'image/' +computerOptions+ '.png');
+            if(computerOptions == 'cut-hand') {
+                backgroundComputer.classList.add('board-games__images-computer-scissors');
+                backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
+            } else if(computerOptions == 'rock-hand') {
+                backgroundComputer.classList.add('board-games__images-computer-rock');
+                backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
+            } else {
+                backgroundComputer.classList.add('board-games__images-computer-paper');
+                backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
+            }
+
+            const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
+            resultInfo.innerHTML = result;
+        }, 1000);
+        
+    });
 });
 
-const playerRock = document.querySelector('.rock');
-playerRock.addEventListener('click', function () {
-    const computerOptions = getComputerOptions();
-    const playerOptions = playerRock.className.toString().split(/\s+/)[2];
-    const result = getResult(computerOptions, playerOptions);
-    console.log('comp : ' +computerOptions);
-    console.log('player : ' +playerOptions);
-    console.log('result : ' +result);
+// const playerScissors = document.querySelector('.scissors');
+// playerScissors.addEventListener('click', function () {
+//     const computerOptions = getComputerOptions();
+//     const playerOptions = playerScissors.className.toString().split(/\s+/)[2];
+//     const result = getResult(computerOptions, playerOptions);
+//     console.log('comp : ' +computerOptions);
+//     console.log('player : ' +playerOptions);
+//     console.log('result : ' +result);
 
-    const backgroundComputer = document.querySelector('.board-games__computer-selection');
-    const imgComputer = document.querySelector('.board-games__images-computer');
+//     const backgroundComputer = document.querySelector('.board-games__computer-selection');
+//     const imgComputer = document.querySelector('.board-games__images-computer');
 
-    if(computerOptions == 'scissors') {
-        imgComputer.setAttribute('src', 'image/cut-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-scissors');
-        backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
-    } else if(computerOptions == 'rock') {
-        imgComputer.setAttribute('src', 'image/rock-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-rock');
-        backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
-    } else {
-        imgComputer.setAttribute('src', 'image/paper-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-paper');
-        backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
-    }
+//     if(computerOptions == 'scissors') {
+//         imgComputer.setAttribute('src', 'image/cut-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-scissors');
+//         backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
+//     } else if(computerOptions == 'rock') {
+//         imgComputer.setAttribute('src', 'image/rock-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-rock');
+//         backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
+//     } else {
+//         imgComputer.setAttribute('src', 'image/paper-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-paper');
+//         backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
+//     }
 
-    const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
-    resultInfo.innerHTML = result;
-});
+//     const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
+//     resultInfo.innerHTML = result;
+// });
 
-const playerPaper = document.querySelector('.paper');
-playerPaper.addEventListener('click', function () {
-    const computerOptions = getComputerOptions();
-    const playerOptions = playerPaper.className.toString().split(/\s+/)[2];
-    const result = getResult(computerOptions, playerOptions);
-    console.log('comp : ' +computerOptions);
-    console.log('player : ' +playerOptions);
-    console.log('result : ' +result);
+// const playerRock = document.querySelector('.rock');
+// playerRock.addEventListener('click', function () {
+//     const computerOptions = getComputerOptions();
+//     const playerOptions = playerRock.className.toString().split(/\s+/)[2];
+//     const result = getResult(computerOptions, playerOptions);
+//     console.log('comp : ' +computerOptions);
+//     console.log('player : ' +playerOptions);
+//     console.log('result : ' +result);
 
-    const backgroundComputer = document.querySelector('.board-games__computer-selection');
-    const imgComputer = document.querySelector('.board-games__images-computer');
+//     const backgroundComputer = document.querySelector('.board-games__computer-selection');
+//     const imgComputer = document.querySelector('.board-games__images-computer');
 
-    if(computerOptions == 'scissors') {
-        imgComputer.setAttribute('src', 'image/cut-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-scissors');
-        backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
-    } else if(computerOptions == 'rock') {
-        imgComputer.setAttribute('src', 'image/rock-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-rock');
-        backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
-    } else {
-        imgComputer.setAttribute('src', 'image/paper-hand.png');
-        backgroundComputer.classList.add('board-games__images-computer-paper');
-        backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
-    }
+//     if(computerOptions == 'scissors') {
+//         imgComputer.setAttribute('src', 'image/cut-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-scissors');
+//         backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
+//     } else if(computerOptions == 'rock') {
+//         imgComputer.setAttribute('src', 'image/rock-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-rock');
+//         backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
+//     } else {
+//         imgComputer.setAttribute('src', 'image/paper-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-paper');
+//         backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
+//     }
 
-    const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
-    resultInfo.innerHTML = result;
-});
+//     const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
+//     resultInfo.innerHTML = result;
+// });
+
+// const playerPaper = document.querySelector('.paper');
+// playerPaper.addEventListener('click', function () {
+//     const computerOptions = getComputerOptions();
+//     const playerOptions = playerPaper.className.toString().split(/\s+/)[2];
+//     const result = getResult(computerOptions, playerOptions);
+//     console.log('comp : ' +computerOptions);
+//     console.log('player : ' +playerOptions);
+//     console.log('result : ' +result);
+
+//     const backgroundComputer = document.querySelector('.board-games__computer-selection');
+//     const imgComputer = document.querySelector('.board-games__images-computer');
+
+//     if(computerOptions == 'scissors') {
+//         imgComputer.setAttribute('src', 'image/cut-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-scissors');
+//         backgroundComputer.classList.remove('board-games__images-computer-rock', 'board-games__images-computer-paper');
+//     } else if(computerOptions == 'rock') {
+//         imgComputer.setAttribute('src', 'image/rock-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-rock');
+//         backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-paper');
+//     } else {
+//         imgComputer.setAttribute('src', 'image/paper-hand.png');
+//         backgroundComputer.classList.add('board-games__images-computer-paper');
+//         backgroundComputer.classList.remove('board-games__images-computer-scissors', 'board-games__images-computer-rock');
+//     }
+
+//     const resultInfo = document.getElementsByClassName("board-games__info-result")[0];
+//     resultInfo.innerHTML = result;
+// });
